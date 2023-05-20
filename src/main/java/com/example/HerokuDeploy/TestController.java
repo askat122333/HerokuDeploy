77ab65de3b0633db6bb661714afa399fe378731c
askat122333 <110248679+askat1223@users.noreply.github.com>
2023-05-20 19:26:47 +0600

    добавил sum

--- src/main/java/com/example/HerokuDeploy/TestController.java
+++ src/main/java/com/example/HerokuDeploy/TestController.java
@@ -1,6 +1,8 @@
 package com.example.HerokuDeploy;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,5 +14,9 @@ public class TestController {
     @GetMapping("/2")
     public String getWord2(){
         return "Heroku test 2";
+    }
+    @GetMapping("/+")
+    public Integer sum (@PathVariable int a,@PathVariable int b) {
+        return a + b ;
     }
 }
